5adbae1944885447f5b7dc9f11a76eb4387a24e7
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-21 23:24:17 -0500

    adds api v1, admin namespaces

--- wb/config/routes.rb
+++ wb/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # API
+  namespace :api do
+    namespace :v1 do
+    end
+  end
+
+  # ADMIN CONSOLE
+  namespace :admin do
+    get '/', to: 'admin#index'
+  end
+
 end
