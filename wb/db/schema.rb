c6a3a7bcc5c9076ff270da2c690c35bac0e9ae6c
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-22 00:40:25 -0500

    migrated

---
+++ wb/db/schema.rb
@@ -0,0 +1,110 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_12_22_053026) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "line_1"
+    t.string "line_2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "addressable_type"
+    t.bigint "addressable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable_type_and_addressable_id"
+  end
+
+  create_table "alternate_contacts", force: :cascade do |t|
+    t.string "name"
+    t.bigint "work_order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["work_order_id"], name: "index_alternate_contacts_on_work_order_id"
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.string "departmentable_type"
+    t.bigint "departmentable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["departmentable_type", "departmentable_id"], name: "index_departments_on_departmentable_type_and_departmentable_id"
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string "address"
+    t.string "emailable_type"
+    t.bigint "emailable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["emailable_type", "emailable_id"], name: "index_emails_on_emailable_type_and_emailable_id"
+  end
+
+  create_table "facilities", force: :cascade do |t|
+    t.string "name"
+    t.boolean "private"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "floors", force: :cascade do |t|
+    t.integer "number"
+    t.bigint "facility_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facility_id"], name: "index_floors_on_facility_id"
+  end
+
+  create_table "phones", force: :cascade do |t|
+    t.string "number"
+    t.string "phoneable_type"
+    t.bigint "phoneable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["phoneable_type", "phoneable_id"], name: "index_phones_on_phoneable_type_and_phoneable_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "floor_id", null: false
+    t.bigint "facility_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facility_id"], name: "index_rooms_on_facility_id"
+    t.index ["floor_id"], name: "index_rooms_on_floor_id"
+  end
+
+  create_table "work_orders", force: :cascade do |t|
+    t.string "status"
+    t.string "discipline"
+    t.text "description"
+    t.text "response"
+    t.bigint "facility_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facility_id"], name: "index_work_orders_on_facility_id"
+    t.index ["room_id"], name: "index_work_orders_on_room_id"
+  end
+
+  add_foreign_key "alternate_contacts", "work_orders"
+  add_foreign_key "floors", "facilities"
+  add_foreign_key "rooms", "facilities"
+  add_foreign_key "rooms", "floors"
+  add_foreign_key "work_orders", "facilities"
+  add_foreign_key "work_orders", "rooms"
+end
