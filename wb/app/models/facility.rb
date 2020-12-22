5254ce75256578d8e9fcc99ff7a41090636823ca
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-22 00:37:54 -0500

    adds address, emails, phones to facility

--- wb/app/models/facility.rb
+++ wb/app/models/facility.rb
@@ -1,4 +1,7 @@
 class Facility < ApplicationRecord
   has_many :floors
   has_many :rooms
+  has_one :address, as: :addressable
+  has_many :emails, as: :emailable
+  has_many :phones, as: :phoneable
 end
