3d937a71aeb72189ba5ebc10fc936f20af47bf4c
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-22 00:39:23 -0500

    adds department, email, phone to alternate contact

--- wb/app/models/alternate_contact.rb
+++ wb/app/models/alternate_contact.rb
@@ -1,3 +1,6 @@
 class AlternateContact < ApplicationRecord
   belongs_to :work_order
+  has_one :department, as: :departmentable
+  has_one :phone, as: :phoneable
+  has_one :email, as: :emailable
 end
