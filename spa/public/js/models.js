e889ce959c91b16300e4a1207dd633a070ff5c76
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-23 11:00:52 -0500

    change to const

--- spa/public/js/models.js
+++ spa/public/js/models.js
@@ -1,6 +1,6 @@
 class Facility {
   constructor(attributes) {
-    let whitelist = ['id', 'name', 'private']
+    const whitelist = ['id', 'name', 'private']
     whitelist.forEach(attr => this[attr] = attributes[attr])
     this['address'] = new Address(attributes['address'])
   }
@@ -35,21 +35,21 @@ class Facility {
 
 class Floor {
   constructor(attributes) {
-    let whitelist = ['id', 'number', 'facility_id']
+    const whitelist = ['id', 'number', 'facility_id']
     whitelist.forEach(attr => this[attr] = attributes[attr])
   }
 }
 
 class Room {
   constructor(attributes) {
-    let whitelist = ['id', 'name', 'floor_id', 'facility_id']
+    const whitelist = ['id', 'name', 'floor_id', 'facility_id']
     whitelist.forEach(attr => this[attr] = attributes[attr])
   }
 }
 
 class Address {
   constructor(attributes) {
-    let whitelist = ['line_1', 'line_2', 'city', 'state', 'zip', 'addressable_id', 'addressable_type']
+    const whitelist = ['line_1', 'line_2', 'city', 'state', 'zip', 'addressable_id', 'addressable_type']
     whitelist.forEach(attr => this[attr] = attributes[attr])
   }
 
