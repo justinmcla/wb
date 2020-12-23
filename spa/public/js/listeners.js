b9b858aa4469b8e7a690645fc0dfaa15465f2614
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-23 10:19:39 -0500

    adds listener for facility name datalist, renders address for input

--- spa/public/js/listeners.js
+++ spa/public/js/listeners.js
@@ -2,3 +2,25 @@ document.addEventListener('DOMContentLoaded', () => {
   Facility.all();
 })
 
+document.addEventListener('input', e => {
+  if (e.target.matches('#facilityName')) {
+    let options = document.querySelector('#facilities').children;
+    let selected = null;
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].value.toLowerCase() == e.target.value.toLowerCase()) {
+        selected = options[i];
+      }
+    }
+    if (selected) {
+      document.querySelector('#instructions').hidden = true
+      let address = Facility.findById(selected.id).address;
+      address.element = address.render();
+      let side = document.querySelector('#side')
+      side.appendChild(address.element)
+    } else {
+      document.querySelector('#instructions').hidden = false;
+      document.querySelectorAll('#facilityAddress').forEach( e => e.remove())
+    }
+  }
+})
+
