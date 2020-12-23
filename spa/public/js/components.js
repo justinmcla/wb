eb0c740a1ed7319acab8f4571288ec5fe48dcade
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-23 15:08:11 -0500

    builds new slideshow func

--- spa/public/js/components.js
+++ spa/public/js/components.js
@@ -18,4 +18,32 @@ class Component {
     return img;
   }
 
+  static newSlideShow(imageArray) {
+    const slideshowItems = document.createElement('ul');
+    slideshowItems.classList.add('uk-slideshow-items');
+
+    imageArray.forEach( img => {
+      img.setAttribute('uk-img', 'target: !.uk-slideshow-items');
+      slideshowItems.appendChild(wrapContent(img, 'li'));
+    });
+
+    const leftArrow = document.createElement('a');
+    leftArrow.classList.add(...['uk-position-center-left', 'uk-position-small', 'uk-hidden-hover']);
+    leftArrow.setAttribute('href', '#');
+    leftArrow.setAttribute('uk-slidenav-previous', '');
+    leftArrow.setAttribute('uk-slideshow-item', 'previous');
+
+    const rightArrow = document.createElement('a');
+    rightArrow.classList.add(...['uk-position-center-right', 'uk-position-small', 'uk-hidden-hover']);
+    rightArrow.setAttribute('href', '#');
+    rightArrow.setAttribute('uk-slidenav-next', '');
+    rightArrow.setAttribute('uk-slideshow-item', 'next');
+
+    const slideshow = document.createElement('div');
+    slideshow.setAttribute('uk-slideshow', '');
+    slideshow.setAttribute('tabindex', '-1');
+    slideshow.append(slideshowItems, leftArrow, rightArrow);
+
+    return slideshow;
+  }
 }
