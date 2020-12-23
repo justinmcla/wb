337e0833c91a69d9a4a00a6f677f663d001a5621
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-22 21:16:56 -0500

    creates models js, builds basic constructors

---
+++ spa/public/js/models.js
@@ -0,0 +1,27 @@
+class Facility {
+  constructor(attributes) {
+    let whitelist = ['id', 'name', 'private']
+    whitelist.forEach(attr => this[attr] = attributes[attr])
+  }
+}
+
+class Floor {
+  constructor(attributes) {
+    let whitelist = ['id', 'number', 'facility_id']
+    whitelist.forEach(attr => this[attr] = attributes[attr])
+  }
+}
+
+class Room {
+  constructor(attributes) {
+    let whitelist = ['id', 'name', 'floor_id', 'facility_id']
+    whitelist.forEach(attr => this[attr] = attributes[attr])
+  }
+}
+
+class Address {
+  constructor(attributes) {
+    let whitelist = ['line_1', 'line_2', 'city', 'state', 'zip', 'addressable_id', 'addressable_type']
+    whitelist.forEach(attr => this[attr] = attributes[attr])
+  }
+}
