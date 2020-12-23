362ddb83f2582602c6e2f9dc52d5f5d4dc547cc8
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-23 10:59:13 -0500

    creates UI class with newCard func

---
+++ spa/public/js/components.js
@@ -0,0 +1,8 @@
+class UI {
+  static newCard() {
+    const card = document.createElement('div');
+    card.classList.add(...['uk-card', 'uk-card-body', 'uk-card-default']);
+    return card;
+  }
+
+}
