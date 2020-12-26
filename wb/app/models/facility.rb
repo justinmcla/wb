cd8f47406799217f9aac5865615c640e7dced6c4
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-26 15:29:17 -0500

    adds images to facilities

--- wb/app/models/facility.rb
+++ wb/app/models/facility.rb
@@ -5,5 +5,7 @@ class Facility < ApplicationRecord
   has_many :emails, as: :emailable
   has_many :phones, as: :phoneable
 
+  has_many_attached :images
+
   scope :public_records, -> { where(private: false) }
 end
