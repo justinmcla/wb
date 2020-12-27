ece59970c3963f5f6295b9e462cd8168152dec87
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-27 10:40:31 -0500

    migrated users

--- wb/db/schema.rb
+++ wb/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_191653) do
+ActiveRecord::Schema.define(version: 2020_12_27_153924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,14 @@ ActiveRecord::Schema.define(version: 2020_12_26_191653) do
     t.index ["aid"], name: "index_rooms_on_aid"
     t.index ["facility_id"], name: "index_rooms_on_facility_id"
     t.index ["floor_id"], name: "index_rooms_on_floor_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "work_orders", force: :cascade do |t|
