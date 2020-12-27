12711a1193a26dfcb9d21067a24a7d65417ff98f
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-27 00:20:56 -0500

    splits event listeners

--- spa/public/js/listeners.js
+++ spa/public/js/listeners.js
@@ -5,103 +5,52 @@ document.addEventListener('DOMContentLoaded', () => {
   Room.all();
 })
 
-document.addEventListener('input', e => {
-  if (e.target.matches('#facilityName')) {
-    let options = document.querySelector('#facilities').children;
-    let selected = null;
-    for (let i = 0; i < options.length; i++) {
-      if (options[i].value.toLowerCase() == e.target.value.toLowerCase()) {
-        selected = options[i];
-      }
-    }
-    if (selected) {
-      document.querySelector('#instructions').hidden = true
-      const facility = Facility.findById(selected.id)
-      UI.renderFacilityCard(facility)
-      document.querySelector('#resetForm').hidden = false
-      UI.loadFloorsByFacilityId(facility.id)
-      const floorId = document.querySelector('#floorNumbers').value
-      UI.loadFloorRooms(floorId)
-    } else {
-      document.querySelector('#resetForm').hidden = true
-      document.querySelector('#instructions').hidden = false;
-      document.querySelectorAll('#facilityAddress').forEach( e => e.remove())
-      const floorNumbers = document.querySelector('#floorNumbers')
-      while(floorNumbers.firstChild) {
-        floorNumbers.removeChild(floorNumbers.lastChild);
-      }
-      while(roomNames.firstChild) {
-        roomNames.removeChild(roomNames.lastChild);
-      }
-    }
-  } else if (e.target.matches('#floorNumbers')) {
-    const roomNames = document.querySelector('#roomNames')
-    while(roomNames.firstChild) {
-      roomNames.removeChild(roomNames.lastChild);
-    }
-    const floorId = e.target.value
-    UI.loadFloorRooms(floorId)
-  } else if (e.target.matches('#imageUpload')) {
-    const indicator = document.querySelector('#fileList')
-    if (e.target.files.length > 1) {
-      indicator.innerHTML = `${e.target.files.length} images selected`;
-    } else {
-      indicator.innerHTML = `${e.target.files.length} image selected`;
-    }
+UI.facilityName().addEventListener('input', e => {
+  const selected = UI.facilityInput(e)
+  if (selected) {
+    UI.handleFacilityInput(selected)
+  } else {
+    UI.resetButton().hidden = true
+    UI.instructions().hidden = false
+    UI.facilityCard().remove()
+    UI.clearFloorNumbers()
+    UI.clearRoomNames()
+    UI.resetAllFormFacilityFields()
   }
 })
 
-document.addEventListener('click', e => {
-  if (e.target.matches('#submitFacilityCode')) {
-    e.preventDefault();
-    const code = document.querySelector('#facilityCode').value
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        'code': code
-      })
-    }
+Floor.container().addEventListener('input', e => {
+  UI.clearRoomNames()
+  UI.loadFloorRooms(e.target.value)
+})
 
-    fetch('http://localhost:3000/api/v1/rooms', options)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
-        UI.handleFacilityCodeSubmission(json);
-      })
-  } else if (e.target.matches('#resetForm')) {
-    const facilityName = document.querySelector('#facilityName')
-    const floorNumbers = document.querySelector('#floorNumbers')
-    const roomNames = document.querySelector('#roomNames')
-    const discipline = document.querySelector('#disciplines')
-    const description = document.querySelector('#description')
-    const facilityCode = document.querySelector('#facilityCode')
-
-
-    facilityName.value = ''
-    while(floorNumbers.firstChild) {
-      floorNumbers.removeChild(floorNumbers.lastChild);
-    }
-    while(roomNames.firstChild) {
-      roomNames.removeChild(roomNames.lastChild);
-    }
-    discipline.value = 'Carpentry'
-    description.value = ''
-    facilityCode.value = ''
-
-    document.querySelector('#instructions').hidden = false
-    document.querySelector('#imageUpload').value = ''
-    document.querySelector('#resetForm').hidden = true
-    document.querySelector('#facilityAddress').remove()
-    document.querySelector('#fileList').innerHTML = ''
-    facilityName.disabled = false
-    floorNumbers.disabled = false
-    roomNames.disabled = false
-    document.querySelector('#facilityCode').disabled = false
-    document.querySelector('#submitFacilityCode').hidden = false
-
+UI.uploads().addEventListener('input', e => {
+  if (e.target.files.length > 1) {
+    UI.fileIndicator().innerHTML = `${e.target.files.length} images selected`;
+  } else {
+    UI.fileIndicator().innerHTML = `${e.target.files.length} image selected`;
   }
+})
+
+UI.facilityCodeButton().addEventListener('click', e => {
+  e.preventDefault();
+  const code = document.querySelector('#facilityCode').value
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      'code': code
+    })
+  }
+
+  fetch('http://localhost:3000/api/v1/rooms', options)
+    .then(response => response.json())
+    .then(json => UI.handleFacilityCodeSubmission(json))
+})
+
+UI.resetButton().addEventListener('click', e => {
+  UI.resetAllFormFields()
 })
