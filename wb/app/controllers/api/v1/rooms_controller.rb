3ef6a61e8ad3ec1fcf2cd73d283e6f4c063ba473
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-27 00:17:42 -0500

    temporarily changes json rendering to better depict final render

--- wb/app/controllers/api/v1/rooms_controller.rb
+++ wb/app/controllers/api/v1/rooms_controller.rb
@@ -13,7 +13,14 @@ class Api::V1::RoomsController < Api::V1::ApiController
   def create
     room = Room.find_by(aid: params[:code])
     if room
-      render json: { id: room.id, name: room.name, facility: room.facility, floor: room.floor, address: room.facility.address }
+      render json: {
+        id:           room.id,
+        name:         room.name,
+        facility_id:  room.facility_id,
+        floor_id:     room.floor_id,
+        facility:     room.facility,
+        floor:        room.floor,
+        address:      room.facility.address }
     end
   end
 end
