df91b16b8b8509f9521d3235873679d198603655
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-27 23:01:33 -0500

    create and destroy logic

--- wb/app/controllers/admin/sessions_controller.rb
+++ wb/app/controllers/admin/sessions_controller.rb
@@ -2,5 +2,17 @@ class Admin::SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.find_by email: email
+    log_in user if user && user.authenticate(password)
+    current_user ? redirect_to(admin_path) : deny_with_alert
+  end
+
+  def destroy
+    session.clear
+    redirect_to admin_login_path
+  end
+
   end
 end
