60ca9fc173e76ae920635b773b04fbd08561f288
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-27 23:02:49 -0500

    current user and redirect helpers

--- wb/app/controllers/application_controller.rb
+++ wb/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    User.find_by_id session[:user_id]
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to admin_login_path unless current_user
+  end
 end
