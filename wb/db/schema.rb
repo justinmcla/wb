151c9f6d1d3068c0445b69d4593c7fdb3a5f9bc3
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-27 23:05:39 -0500

    updates migration

--- wb/db/schema.rb
+++ wb/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_153924) do
+ActiveRecord::Schema.define(version: 2020_12_27_200408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,9 +114,9 @@ ActiveRecord::Schema.define(version: 2020_12_27_153924) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "work_orders", force: :cascade do |t|
