7d68dacee1051b9ee0c98cdc8d6cb8058f316864
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-28 22:17:27 -0500

    adds user assoc

--- wb/app/models/facility.rb
+++ wb/app/models/facility.rb
@@ -5,6 +5,7 @@ class Facility < ApplicationRecord
   has_many :emails, as: :emailable
   has_many :phones, as: :phoneable
 
+  belongs_to :user
   has_many_attached :images
 
   scope :public_records, -> { where(private: false) }
