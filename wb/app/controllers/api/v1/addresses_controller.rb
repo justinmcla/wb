98e547955ea377b0da063979ae94cac7ee624e41
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-29 23:07:15 -0500

    use serializer

--- wb/app/controllers/api/v1/addresses_controller.rb
+++ wb/app/controllers/api/v1/addresses_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::AddressesController < Api::V1::ApiController
   def index
-    render json: addresses,  except: [:created_at, :updated_at]
     addresses = Address.joins("INNER JOIN facilities ON addresses.addressable_id = facilities.id")
                        .where(addresses: { addressable_type: 'Facility' })
                        .where(facilities: { private: false })
+    render json: AddressSerializer.new(addresses)
   end
 end
