c893c6a5be923812b9860fd76b818de7cae6ec6e
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-29 23:11:41 -0500

    set confirmation and status before a work order is created

--- wb/app/models/work_order.rb
+++ wb/app/models/work_order.rb
@@ -1,5 +1,23 @@
 class WorkOrder < ApplicationRecord
   to_param :confirmation
+  
+  before_create :set_confirmation
+  before_create :set_status
+
   belongs_to :facility
   belongs_to :room
+
+  private
+
+  def set_confirmation
+    # ~17m possibilities
+    self.confirmation = SecureRandom.hex 6
+    while WorkOrder.find_by confirmation: self.confirmation
+      self.confirmation = SecureRandom.hex 6
+    end
+  end
+
+  def set_status
+    self.status = 'pending'
+  end
 end
