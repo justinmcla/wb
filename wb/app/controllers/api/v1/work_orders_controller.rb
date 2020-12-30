1758ab6b54ac2896620e8f0113579ea10a2797d7
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-29 23:10:59 -0500

    creates work order api controller, adds logic for show action

---
+++ wb/app/controllers/api/v1/work_orders_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::WorkOrdersController < Api::V1::ApiController
+  def show
+    work_order = set_work_order
+    if work_order
+      render json: WorkOrderSerializer.new(work_order, include: [:facility, :room])
+    else
+      render json: {errors: 'Work order not found.',
+        status: :unprocessable_entity}
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def set_work_order
+    WorkOrder.find_by confirmation: params[:confirmation]
+  end
+end
