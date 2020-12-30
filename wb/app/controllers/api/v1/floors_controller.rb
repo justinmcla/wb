b5e0e2e169fd30c867e1e9ea102752703978a1c3
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-29 23:08:22 -0500

    rework AR query to grab floors of public facilities

--- wb/app/controllers/api/v1/floors_controller.rb
+++ wb/app/controllers/api/v1/floors_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::FloorsController < Api::V1::ApiController
   def index
-    facilities = Facility.public_records
-    floors = []
-    facilities.each do |f|
-      f.floors.each do |fl|
-        floors.push fl
-      end
-    end
     render json: floors, include: :facility, except: [:created_at, :updated_at]
+    floors = Floor.includes(:facility).where(facilities: { private: false })
   end
 end
