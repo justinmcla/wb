cc36b423db02b6c4ab3dcca17601109c3651cf5c
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-29 23:08:30 -0500

    use serializer

--- wb/app/controllers/api/v1/floors_controller.rb
+++ wb/app/controllers/api/v1/floors_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FloorsController < Api::V1::ApiController
   def index
-    render json: floors, include: :facility, except: [:created_at, :updated_at]
     floors = Floor.includes(:facility).where(facilities: { private: false })
+    render json: FloorSerializer.new(floors)
   end
 end
