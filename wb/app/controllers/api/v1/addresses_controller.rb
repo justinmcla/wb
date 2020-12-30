9283e0070a5430263f44b902128684d2b10c9f48
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-29 23:07:06 -0500

    rework AR query to grab addresses of public facilities

--- wb/app/controllers/api/v1/addresses_controller.rb
+++ wb/app/controllers/api/v1/addresses_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::AddressesController < Api::V1::ApiController
   def index
-    facilities = Facility.public_records
-    addresses = []
-    facilities.each do |f|
-      addresses.push f.address
-    end
     render json: addresses,  except: [:created_at, :updated_at]
+    addresses = Address.joins("INNER JOIN facilities ON addresses.addressable_id = facilities.id")
+                       .where(addresses: { addressable_type: 'Facility' })
+                       .where(facilities: { private: false })
   end
 end
