18992bcc737ba1107f981fd1b359e8ed392b7558
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-29 23:09:11 -0500

    rework AR query to grab rooms of public facilities

--- wb/app/controllers/api/v1/rooms_controller.rb
+++ wb/app/controllers/api/v1/rooms_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::RoomsController < Api::V1::ApiController
   def index
-    facilities = Facility.public_records
-    rooms = []
-    facilities.each do |f|
-      f.rooms.each do |r|
-        rooms.push r
-      end
-    end
     render json: rooms, except: [:created_at, :updated_at]
+    rooms = Room.includes(:facility).where(facilities: { private: false })
   end
 
   def create
