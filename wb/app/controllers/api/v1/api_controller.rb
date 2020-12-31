bd179dcea397dfe6fd71c54da163a77c0b95ffc8
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-31 02:13:31 -0500

    adds authenticate token methods

--- wb/app/controllers/api/v1/api_controller.rb
+++ wb/app/controllers/api/v1/api_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::ApiController < ActionController::API
+
+  private
+
+  def authenticate_token
+    payload = JsonWebToken.decode(auth_token)
+    @code   = payload['code']
+  rescue JWT::ExpiredSignature
+    render json: { errors: 'Expired token', status: :unauthorized }
+  rescue JWT::DecodeError
+    render json: { errors: 'Invalid token', status: :unauthorized }
+  end
+
+  def auth_token
+    @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
+  end
 end
