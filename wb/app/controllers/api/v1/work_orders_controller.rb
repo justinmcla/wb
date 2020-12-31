77c20fd770c1e5d6a374e08d2f72988013964e35
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-31 02:14:46 -0500

    removes obsolete methods

--- wb/app/controllers/api/v1/work_orders_controller.rb
+++ wb/app/controllers/api/v1/work_orders_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::WorkOrdersController < Api::V1::ApiController
   before_action :authenticate_token, only: :show
 
   def show
-    work_order = set_work_order
+    work_order = WorkOrder.find_by confirmation: @code
     if work_order
       render json: WorkOrderSerializer.new(work_order, include: [:facility, :room])
     else
@@ -14,9 +14,4 @@ class Api::V1::WorkOrdersController < Api::V1::ApiController
   def create
   end
 
-  private
-
-  def set_work_order
-    WorkOrder.find_by confirmation: params[:confirmation]
-  end
 end
