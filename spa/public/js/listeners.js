f89d6cf19becd6d58f72873e44dcf8db109c2624
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-31 02:08:29 -0500

    cleans getters

--- spa/public/js/listeners.js
+++ spa/public/js/listeners.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   Room.all();
 })
 
-UI.facilityName().addEventListener('input', e => {
+UI.get('#facilityName').addEventListener('input', e => {
   const selected = UI.facilityInput(e)
   if (selected) {
     UI.handleFacilityInput(selected)
@@ -15,23 +15,28 @@ UI.facilityName().addEventListener('input', e => {
     if (UI.facilityCard()) { UI.facilityCard().remove() }
     UI.clearFloorNumbers()
     UI.clearRoomNames()
+    UI.get('#resetFormButton').hidden = true
+    UI.get('#instructions').hidden = false
+    if (UI.get('#facilityCard')) { UI.get('#facilityCard').remove() }
+    Floor.container().innerHTML = ''
+    Room.container().innerHTML  = ''
   }
 })
 
 Floor.container().addEventListener('input', e => {
-  UI.clearRoomNames()
-  UI.loadFloorRooms(e.target.value)
+  Room.container().innerHTML = ''
+  Room.loadByFloor(e.target.value)
 })
 
-UI.uploads().addEventListener('input', e => {
+UI.get('#imageUpload').addEventListener('input', e => {
   if (e.target.files.length > 1) {
-    UI.fileIndicator().innerHTML = `${e.target.files.length} images selected`;
+    UI.get('#fileList').innerHTML = `${e.target.files.length} images selected`;
   } else {
-    UI.fileIndicator().innerHTML = `${e.target.files.length} image selected`;
+    UI.get('#fileList').innerHTML = `${e.target.files.length} image selected`;
   }
 })
 
-UI.facilityCodeForm().addEventListener('submit', e => {
+UI.get('#facilityCodeForm').addEventListener('submit', e => {
   e.preventDefault();
   const code = document.querySelector('#facilityCode').value
 UI.resetButton().addEventListener('click', e => {
