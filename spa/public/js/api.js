4b26ece8ecf6e8e58c678055029daddee50729b4
Justin McLawhorn <jwmclawhorn@gmail.com>
2020-12-31 02:06:54 -0500

    doc

--- spa/public/js/api.js
+++ spa/public/js/api.js
@@ -1,4 +1,10 @@
 class API {
+
+  /*
+  Below are the SPA-wide API endpoints. This provides a central
+  location to update all endpoints once the API server is deployed.
+  */
+
   static base() {
     return 'http://localhost:3000/api/v1'
   }
