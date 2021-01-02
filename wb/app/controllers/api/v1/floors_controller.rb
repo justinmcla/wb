138d6034c7cf2e63b8e17aee8745a6690961d4ef
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-02 10:36:37 -0500

    simplify how floors are rendered, minimize db queries

--- wb/app/controllers/api/v1/floors_controller.rb
+++ wb/app/controllers/api/v1/floors_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FloorsController < Api::V1::ApiController
   def index
     floors = Floor.includes(:facility).where(facilities: { private: false })
-    render json: FloorSerializer.new(floors)
+    render json: floors, except: %i[created_at updated_at]
   end
 end
