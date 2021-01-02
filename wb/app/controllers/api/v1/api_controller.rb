bbb813df23bc5940b83f01b694ac23a9752c9f19
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-02 10:31:22 -0500

    adds default render methods for errors/tokens

--- wb/app/controllers/api/v1/api_controller.rb
+++ wb/app/controllers/api/v1/api_controller.rb
@@ -1,17 +1,37 @@
 class Api::V1::ApiController < ActionController::API
-
   private
 
   def authenticate_token
     payload = JsonWebToken.decode(auth_token)
     @code   = payload['code']
   rescue JWT::ExpiredSignature
-    render json: { errors: 'Credentials expired', status: :unauthorized } and return
+    render_expired and return
   rescue JWT::DecodeError
-    render json: { errors: 'Invalid credentials', status: :unauthorized } and return
+    render_invalid and return
   end
 
   def auth_token
-    @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
+    @auth_token ||= request.headers.fetch('Authorization', '').split(' ').last
+  end
+
+  def render_token(code)
+    token = JsonWebToken.encode({ code: code })
+    render json: { token: token, status: 200 }
+  end
+
+  def render_expired
+    render json: { errors: 'Credentials expired', status: 401 }
+  end
+
+  def render_invalid
+    render json: { errors: 'Invalid credentials', status: 401 }
+  end
+
+  def render_no_record
+    render json: { errors: 'Record not found', status: 422 }
+  end
+
+  def render_errors(resource)
+    render json: { errors: resource.errors.full_messages, status: 422 }
   end
 end
