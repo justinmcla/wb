fb4c0e056defb5731d9f0251a1a0749d2e86fe3e
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-02 10:36:17 -0500

    simplify how facilities are rendered, minimize active record queries

--- wb/app/controllers/api/v1/facilities_controller.rb
+++ wb/app/controllers/api/v1/facilities_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FacilitiesController < Api::V1::ApiController
   def index
     facilities = Facility.public_records
-    render json: FacilitySerializer.new(facilities)
+    render json: facilities, except: %i[private created_at updated_at]
   end
 end
