ec73241baf353f019b03d27e13f80c1560cc030d
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-02 10:35:50 -0500

    beautifier, update logic to use new business and render methods

--- wb/app/controllers/api/v1/authentication_controller.rb
+++ wb/app/controllers/api/v1/authentication_controller.rb
@@ -1,19 +1,17 @@
 class Api::V1::AuthenticationController < Api::V1::ApiController
   def create
-    if record.nil?
-      render json: { errors: 'Record not found', status: :unprocessable_entity }
-    elsif record && record.authenticate(strong_params[:password])
-      render json: { token: JsonWebToken.encode({ code: strong_params[:code] }), status: :ok }
-    else
-      render json: { errors: 'Invalid credentials', status: :unauthorized }
-    end
+    record = handle_params
+    render_no_record and return unless record
+    render_invalid   and return unless record.authenticate(strong_params[:password])
+
+    render_token(strong_params[:code])
   end
 
   private
 
-  def record
+  def handle_params
     case strong_params[:request_type]
-    when 'room' then Room.find_by aid: params[:code]
+    when 'room'      then Room.find_by aid: params[:code]
     when 'workOrder' then WorkOrder.find_by confirmation: params[:code]
     end
   end
@@ -21,5 +19,4 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
   def strong_params
     params.permit(:request_type, :code, :confirmation, :password)
   end
-
 end
