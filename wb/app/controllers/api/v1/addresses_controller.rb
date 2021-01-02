522a000514470144b3bf3587f4b0d2ced9949819
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-02 10:30:53 -0500

    beautifier, simplifies how addresses are rendered

--- wb/app/controllers/api/v1/addresses_controller.rb
+++ wb/app/controllers/api/v1/addresses_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::AddressesController < Api::V1::ApiController
   def index
-    addresses = Address.joins("INNER JOIN facilities ON addresses.addressable_id = facilities.id")
+    addresses = Address.joins('INNER JOIN facilities ON addresses.addressable_id = facilities.id')
                        .where(addresses: { addressable_type: 'Facility' })
                        .where(facilities: { private: false })
-    render json: AddressSerializer.new(addresses)
+    render json: addresses, except: %i[created_at updated_at]
   end
 end
