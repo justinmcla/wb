8bb7cd337dc794f5e2e689d0af88047f4c4e5d30
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-01 22:54:07 -0500

    work order create and update logic

--- wb/app/controllers/api/v1/work_orders_controller.rb
+++ wb/app/controllers/api/v1/work_orders_controller.rb
@@ -12,6 +12,42 @@ class Api::V1::WorkOrdersController < Api::V1::ApiController
   end
 
   def create
+    if is_private strong_params['facility_id']
+      authenticate_token
+      return unless @code
+    end
+    work_order = WorkOrder.new(strong_params)
+    work_order.password = work_order.confirmation
+    if work_order.save
+      render json: { data: WorkOrderSerializer.new(work_order,
+        include: [:facility, :room]), status: :ok, token: JsonWebToken.encode({ code: work_order.confirmation }) }
+    else
+      render json: { errors: work_order.errors.full_messages, status: :unprocessable_entity }
+    end
+  end
+
+  def update
+    work_order = WorkOrder.find_by confirmation: @code
+    if work_order
+      work_order.update(patch_params)
+      render json: { token: JsonWebToken.encode({ code: work_order.confirmation }), message: 'updated', status: :ok }
+    else
+      render json: { errors: 'Work order not found.', status: :unprocessable_entity }
+    end
+  end
+
+  private
+
+  def strong_params
+    params.permit('facility_id', 'room_id', 'discipline', 'description', 'password', 'confirmation', 'images')
+  end
+
+  def patch_params
+    params.permit('password')
+  end
+
+  def is_private facility
+    Facility.find_by_id(facility).private
   end
 
 end
