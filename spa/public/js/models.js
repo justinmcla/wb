6d14fdb60868f883ef19123bdb17888186d69177
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-03 03:44:53 -0500

    refactor and cleans

--- spa/public/js/models.js
+++ spa/public/js/models.js
@@ -14,7 +14,7 @@ class Facility {
 
   // Renders each facility in collection and appends to the parent node.
   static load() {
-    this.collection().forEach( facility => {
+    this.collection().forEach(facility => {
       this.container().appendChild(facility.render())
     })
   }
@@ -24,15 +24,15 @@ class Facility {
     return this.list ||= []
   }
 
-  // Fetches all public facilities, creates objs
+  // Fetches all public facilities, creates objs, and loads them to DOM
   static all() {
     return fetch(API.facilities())
-    .then(res => res.json())
-    .then(facilities => {
-      facilities.forEach(attr => new Facility(attr))
-      this.load()
-      return this.collection()
-    })
+      .then(res => res.json())
+      .then(facilities => {
+        facilities.forEach(attr => new Facility(attr))
+        this.load()
+        return this.collection()
+      })
   }
 
   // Finds element in cache that matches passed in ID
@@ -40,23 +40,20 @@ class Facility {
     return this.collection().find(facility => facility.id == id)
   }
 
+  // Finds element in cache that matches passed in name
   static findByName(name) {
     return this.collection().find(facility => facility.name == name)
   }
 
   // Finds Facility object if exists, or creates new one
   static findOrCreate(object) {
-    if (Facility.findById(object.id)) {
-      return Facility.findById(object.id)
-    } else {
-      return new Facility(object)
-    }
+    return Facility.findById(object.id) || new Facility(object)
   }
 
   // Returns facility option node to be appended to DOM
   render() {
     this.element ||= document.createElement('option')
-    this.element.id = this.id
+    this.element.id    = this.id
     this.element.value = this.name
     return this.element
     // <option id = '#' value = 'Some Facility'></option>
@@ -67,8 +64,7 @@ class Facility {
     this.card ||= document.createElement('div')
     this.card.classList.add(...['uk-card', 'uk-card-body', 'uk-card-default'])
     this.card.id = 'facilityCard'
-    const address = Address.findByFacilityId(this.id).render()
-    this.card.appendChild(address)
+    this.card.appendChild(Address.findByFacilityId(this.id).render())
     return this.card
   }
 }
@@ -82,12 +78,12 @@ class Floor {
     Floor.collection().push(this)
   }
 
-   // Returns Floor cache
+  // Returns Floor cache
   static collection() {
     return this.list ||= []
   }
 
-   // Returns the DOM parent node element
+  // Returns the DOM parent node element
   static container() {
     return this.c ||= document.querySelector('#floorNumbers')
   }
@@ -95,11 +91,11 @@ class Floor {
   // Fetches floors for public facilities
   static all() {
     return fetch(API.floors())
-    .then(res => res.json())
-    .then(floors => {
-      floors.forEach(attr => new Floor(attr))
-      return this.collection()
-    })
+      .then(res => res.json())
+      .then(floors => {
+        floors.forEach(attr => new Floor(attr))
+        return this.collection()
+      })
   }
 
   // Finds element in cache that matches passed in ID
@@ -109,24 +105,20 @@ class Floor {
 
   // Loads floors based on selected facility
   static loadByFacilityId(id) {
-    const floors = this.collection().filter( f => f.facility_id == id )
-    floors.forEach( floor => this.container().appendChild(floor.render()) )
+    const floors = this.collection().filter(f => f.facility_id == id)
+    floors.forEach(floor => this.container().appendChild(floor.render()))
   }
 
   // Finds or creates a floor object based on passed in object
   static findOrCreate(object) {
-    if (Floor.findById(object.id)) {
-      return Floor.findById(object.id)
-    } else {
-      return new Floor(object)
-    }
+    return Floor.findById(object.id) || new Floor(object)
   }
 
   // Returns floor node element to be appended to DOM
   render() {
     this.element ||= document.createElement('option')
-    this.element.id = this.id
-    this.element.value = this.id
+    this.element.id        = this.id
+    this.element.value     = this.id
     this.element.innerHTML = this.number
     return this.element // <option id = '#' value = '#'>#</option>
   }
@@ -144,11 +136,11 @@ class Room {
   // Fetches rooms for public facilities, creates new rooms
   static all() {
     return fetch(API.rooms())
-    .then(res => res.json())
-    .then(rooms => {
-      rooms.forEach(attr => new Room(attr))
-      return this.collection()
-    })
+      .then(res => res.json())
+      .then(rooms => {
+        rooms.forEach(attr => new Room(attr))
+        return this.collection()
+      })
   }
 
   // Returns room cache
@@ -168,28 +160,24 @@ class Room {
 
   // Finds or creates new Room based on passed in object
   static findOrCreate(object) {
-    if (Room.findById(object.id)) {
-      return Room.findById(object.id)
-    } else {
-      return new Room(object)
-    }
+    return Room.findById(object.id) || new Room(object)
   }
 
   // Loads rooms based on selected floor
   static loadByFloor(id) {
-    const rooms = Room.collection().filter( r => r.floor_id == id )
-    rooms.forEach( room => Room.container().appendChild(room.render()) )
+    const rooms = Room.collection().filter(r => r.floor_id == id)
+    rooms.forEach(room => Room.container().appendChild(room.render()))
   }
 
   // Grabs room information using JWT
   static show(code) {
     return fetch(API.rooms(code), {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': JsonWebToken.get()
-      }
-    }).then(res => res.json())
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': JsonWebToken.get()
+        }
+      }).then(res => res.json())
       .then(json => {
         if (json.status == 422) {
           return Promise.reject(json.errors)
@@ -206,8 +194,8 @@ class Room {
   // Returns option node element to be appended to the DOM
   render() {
     this.element ||= document.createElement('option')
-    this.element.id = this.id
-    this.element.value = this.id
+    this.element.id        = this.id
+    this.element.value     = this.id
     this.element.innerHTML = this.name
     this.element.setAttribute('data-floor-id', this.floor_id)
     this.element.setAttribute('data-facility-id', this.facility_id)
@@ -237,11 +225,7 @@ class Address {
 
   // Finds or creates Address objects based on passed in object
   static findOrCreate(object) {
-    if (Address.findById(object.id)) {
-      return Address.findById(object.id)
-    } else {
-      return new Address(object)
-    }
+    return Address.findById(object.id) || new Address(object)
   }
 
   // Returns address cache
@@ -266,34 +250,23 @@ class Address {
 
   // Returns paragraph element to be appended to DOM
   render() {
-    if (this.element) {
-      return this.element
-    } else {
-      this.element ||= document.createElement('p')
-      this.element.id = this.id
-
-      const lineOne = document.createElement('span')
-      lineOne.textContent = this.line_1
-      this.element.append(lineOne, document.createElement('br'))
-
-      // Conditionally add second line if Address Line 2 exists
-      if (this['line_2']) {
-        const lineTwo = document.createElement('span')
-        lineTwo.textContent = this.line_2
-        this.element.append(lineTwo, document.createElement('br'))
-      }
-
-      const lineThree = document.createElement('span')
-      lineThree.textContent = `${this.city}, ${this.state} ${this.zip}`
-      this.element.append(lineThree, document.createElement('br'))
-
-      return this.element
-      // <p id = '#'>
-      //   Sample Address
-      //   Line 2
-      //   City, State Zip
-      // </p>
-    }
+    if (this.element) { return this.element }
+    this.element    = document.createElement('p')
+    this.element.id = this.id
+    if (this.line_2) { this.element.innerHTML = `
+        <span>${this.line_1}</span><br>
+        <span>${this.line_2}</span><br>
+        <span>${this.city}, ${this.state} ${this.zip}</span><br>
+    `} else { this.element.innerHTML = `
+        <span>${this.line_1}</span><br>
+        <span>${this.city}, ${this.state} ${this.zip}</span><br>
+    `}
+    return this.element
+    // <p id = '#'>
+    //   Sample Address
+    //   Line 2
+    //   City, State Zip
+    // </p>
   }
 }
 
@@ -301,7 +274,10 @@ class WorkOrder {
 
   // Creates and caches work order
   constructor(attributes) {
-    let whitelist = ['id', 'status', 'discipline', 'description', 'response', 'facility_id', 'room_id', 'confirmation']
+    let whitelist = [
+      'id', 'status', 'discipline', 'description',
+      'response', 'facility_id', 'room_id', 'confirmation'
+    ]
     whitelist.forEach(attr => this[attr] = attributes[attr])
     WorkOrder.collection().push(this)
   }
@@ -311,46 +287,48 @@ class WorkOrder {
     return this.list ||= []
   }
 
+  // Grabs work order data from server
   static show(code) {
     return fetch(API.workOrders(code), {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': JsonWebToken.get()
-      }
-    }).then(res => res.json())
-    .then(json => {
-      if(json.status == 'unprocessable_entity') {
-        return Promise.reject(json.errors)
-      } else {
-        return json
-      }
-    }).catch(error => {
-      console.error(error)
-      document.querySelector('#passwordError').innerHTML = error
-      document.querySelector('#password').classList.add('uk-form-danger')
-    })
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': JsonWebToken.get()
+        }
+      }).then(res => res.json())
+      .then(json => {
+        if (json.status == 422) {
+          return Promise.reject(json.errors)
+        } else {
+          return json
+        }
+      }).catch(error => {
+        console.error(error)
+        document.querySelector('#passwordError').innerHTML = error
+        document.querySelector('#password').classList.add('uk-form-danger')
+      })
   }
 
+  // Posts form data to server to be created, then creates new object on client side
   static create(data) {
     return fetch(API.workOrders(), {
-      method: 'POST',
-      headers: {
-        'Authorization': JsonWebToken.get()
-      }, body: data
-    }).then(res => res.json())
-    .then(json => {
-      if(json.status == 'ok') {
-        JsonWebToken.set(json.token)
-        const createdWorkOrder = new WorkOrder(json.data.data.attributes)
-        return createdWorkOrder
-      } else {
-        return Promise.reject(json.errors)
-      }
-    }).catch(error => {
-      console.error(error)
-      UI.get('#alertError').innerHTML = error
-      UIkit.modal(UI.get('#alertModal')).show()
-    })
+        method: 'POST',
+        headers: {
+          'Authorization': JsonWebToken.get()
+        },
+        body: data
+      }).then(res => res.json())
+      .then(json => {
+        if (json.status == 200) {
+          JsonWebToken.set(json.token)
+          return new WorkOrder(json.data.data.attributes)
+        } else {
+          return Promise.reject(json.errors)
+        }
+      }).catch(error => {
+        console.error(error)
+        UI.get('#alertError').innerHTML = error
+        UIkit.modal(UI.get('#alertModal')).show()
+      })
   }
 }
