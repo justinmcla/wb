81e37dcaca12b53e03fe54807f36027800bb5dad
Justin McLawhorn <jwmclawhorn@gmail.com>
2021-01-03 12:59:15 -0500

    restricted routes to controller actions

--- wb/config/routes.rb
+++ wb/config/routes.rb
@@ -1,26 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/admin')
 
   # API
   namespace :api do
     namespace :v1 do
-      resources :facilities
-      resources :floors
-      resources :rooms, param: :aid
-      resources :addresses
-      resources :work_orders, param: :confirmation
-      post '/auth', to: 'authentication#create'
+      resources :facilities, only: :index
+      resources :floors, only: :index
+      resources :rooms, only: %i[index show], param: :aid
+      resources :addresses, only: :index
+      resources :work_orders, only: %i[show create update], param: :confirmation
+      resource  :auth, only: :create, controller: 'authentication'
     end
   end
 
   # ADMIN CONSOLE
   namespace :admin do
-    get '/', to: 'admin#index'
+    root to: 'admin#index'
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
-    resources :users, only: [:create]
+    resources :users, only: :create
     resources :facilities
   end
-
 end
